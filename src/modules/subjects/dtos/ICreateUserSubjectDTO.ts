e447a3016782e7dcadfa9bdffe3182e6692ba881
kortkamp <marcelusmedius@gmail.com>
2022-06-04 09:39:49 -0300

    feat: get type from use role name

--- src/modules/subjects/dtos/ICreateUserSubjectDTO.ts
+++ src/modules/subjects/dtos/ICreateUserSubjectDTO.ts
@@ -1,6 +1,9 @@
+import { IUserSubjectType } from '../models/IUserSubject';
+
 interface ICreateUserSubjectDTO {
   user_id: string;
   subject_id: string;
+  type: IUserSubjectType;
 }
 
 export { ICreateUserSubjectDTO };
--- src/modules/subjects/services/CreateUserSubjectService.ts
+++ src/modules/subjects/services/CreateUserSubjectService.ts
@@ -1,25 +1,53 @@
+import { IRolesRepository } from '@modules/roles/repositories/IRolesRepository';
+import { IUsersRepository } from '@modules/users/repositories/IUsersRepository';
 import { inject, injectable } from 'tsyringe';
 
 import ErrorsApp from '@shared/errors/ErrorsApp';
 
 import { ICreateUserSubjectDTO } from '../dtos/ICreateUserSubjectDTO';
+import { IUserSubjectType } from '../models/IUserSubject';
 import { IUserSubjectsRepository } from '../repositories/IUserSubjectsRepository';
 
 @injectable()
 class CreateUserSubjectService {
   constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('RolesRepository')
+    private rolesRepository: IRolesRepository,
+
     @inject('UserSubjectsRepository')
     private userSubjectsRepository: IUserSubjectsRepository,
   ) {}
 
   public async execute(data: ICreateUserSubjectDTO) {
+    const user = await this.usersRepository.findById(data.user_id);
+
+    if (!user) {
+      throw new ErrorsApp('User not found', 404);
+    }
+
+    const role = await this.rolesRepository.findById(user.role_id);
+
     const userSubjectExists = await this.userSubjectsRepository.findByIds(data);
 
     if (userSubjectExists) {
       throw new ErrorsApp('Relation already exists', 409);
     }
 
-    const userSubject = await this.userSubjectsRepository.create(data);
+    if (
+      !Object.values(IUserSubjectType).includes(role.name as IUserSubjectType)
+    ) {
+      throw new ErrorsApp(
+        `Role: ${role.name} not authorized to be associate to a Subject`,
+        409,
+      );
+    }
+    const userSubject = await this.userSubjectsRepository.create({
+      ...data,
+      type: role.name as IUserSubjectType,
+    });
 
     return userSubject;
   }
