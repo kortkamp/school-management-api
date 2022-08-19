aa35bf79e3df8e117d1d05c522cb21e1f5660428
kortkamp <marcelusmedius@gmail.com>
2022-08-19 08:25:59 -0300

    feat: throw error on no school id provided

--- src/modules/routines/services/ListRoutineGroupsService.ts
+++ src/modules/routines/services/ListRoutineGroupsService.ts
@@ -1,5 +1,7 @@
 import { inject, injectable } from 'tsyringe';
 
+import ErrorsApp from '@shared/errors/ErrorsApp';
+
 import { IRoutineGroupsRepository } from '../repositories/IRoutineGroupsRepository';
 
 @injectable()
@@ -9,6 +11,9 @@ class ListRoutineGroupsService {
     private routineGroupsRepository: IRoutineGroupsRepository,
   ) {}
   public async execute(school_id: string) {
+    if (!school_id) {
+      throw new ErrorsApp('Id da escola não informado', 400);
+    }
     const routineGroups = await this.routineGroupsRepository.getAll(school_id);
 
     return routineGroups;
