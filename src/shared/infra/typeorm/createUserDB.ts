69e9be0f44d2fcb4bf72e95d8f10cd178b95889b
kortkamp <marcelusmedius@gmail.com>
2022-10-11 16:57:33 -0300

    chore: add prepare db script

---
+++ src/shared/infra/typeorm/createUserDB.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+import { AppDataSource } from '.';
+
+async function create() {
+  await AppDataSource.initialize();
+
+  await AppDataSource.query(
+    `CREATE USER ${process.env.DATABASE_USER} WITH LOGIN PASSWORD '${process.env.DATABASE_PASSWORD}';
+     GRANT ALL PRIVILEGES ON DATABASE ${process.env.DATABASE_DB} to ${process.env.DATABASE_USER};
+     GRANT ALL ON schema public TO ${process.env.DATABASE_USER};
+     GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public to ${process.env.DATABASE_USER}
+    `,
+  );
+
+  await AppDataSource.destroy();
+}
+
+create().then(() => console.log('User DB Created!'));
