ac3f4583f805ab226fc10562c4cbaf8a54e526f1
kortkamp <marcelusmedius@gmail.com>
2022-11-06 09:01:34 -0300

    feat: change validation

--- src/modules/persons/infra/http/validations/persons.validation.ts
+++ src/modules/persons/infra/http/validations/persons.validation.ts
@@ -4,8 +4,8 @@ export const createPersonValidate = celebrate(
   {
     [Segments.BODY]: {
       name: Joi.string().min(3).max(100).required(),
-      cpf: Joi.string().length(11).empty('').default(null).allow(null),
-      rg: Joi.string().min(2).max(20).empty('').default(null).allow(null),
+      cpf: Joi.string().length(11),
+      rg: Joi.string().min(2).max(20),
       sex: Joi.string().valid('M', 'F').required(),
       birth: Joi.string().empty('').default(null).allow(null),
       role_id: Joi.string().uuid().empty('').default(null).allow(null),
@@ -49,11 +49,21 @@ export const updatePersonValidate = celebrate(
       id: Joi.string().uuid().required(),
     },
     [Segments.BODY]: {
-      name: Joi.string().min(3).max(100),
-      cpf: Joi.string().length(11).empty('').default(null).allow(null),
-      rg: Joi.string().min(2).max(20).empty('').default(null).allow(null),
-      sex: Joi.string().valid('M', 'F'),
-      birth: Joi.string().isoDate(),
+      name: Joi.string().min(3).max(100).required(),
+      cpf: Joi.string().length(11),
+      rg: Joi.string().min(2).max(20),
+      sex: Joi.string().valid('M', 'F').required(),
+      birth: Joi.string().isoDate().required(),
+      addresses: Joi.array().items({
+        id: Joi.string().uuid(),
+        street: Joi.string().min(3).max(100).required(),
+        number: Joi.string().min(1).max(20).required(),
+        complement: Joi.string().max(30).required().allow(''),
+        district: Joi.string().min(1).max(20).required(),
+        city: Joi.string().min(1).max(40).required(),
+        state: Joi.string().length(2).required(),
+        CEP: Joi.string().length(8).required(),
+      }),
     },
   },
   {
