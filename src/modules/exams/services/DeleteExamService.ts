94c2599aa54c02f6f70cd3f61b3f39ef3f200c68
kortkamp <marcelusmedius@gmail.com>
2022-11-13 10:23:40 -0300

    feat: verify teacher before removing exam

--- src/modules/exams/services/DeleteExamService.ts
+++ src/modules/exams/services/DeleteExamService.ts
@@ -1,19 +1,42 @@
+import { ITeachersRepository } from '@modules/teachers/repositories/ITeachersRepository';
 import { inject, injectable } from 'tsyringe';
 
 import ErrorsApp from '@shared/errors/ErrorsApp';
 
 import { IExamsRepository } from '../repositories/IExamsRepository';
 
+interface IRequest {
+  authUserId: string;
+  schoolId: string;
+  examId: string;
+}
+
 @injectable()
 class DeleteExamService {
   constructor(
     @inject('ExamsRepository')
     private examsRepository: IExamsRepository,
+
+    @inject('TeachersRepository')
+    private teachersRepository: ITeachersRepository,
   ) {}
-  public async execute(examId: string) {
-    const exam = await this.examsRepository.findById(examId);
+  public async execute({ examId, authUserId, schoolId }: IRequest) {
+    const teacher = await this.teachersRepository.findByUser(
+      schoolId,
+      authUserId,
+    );
+
+    if (!teacher) {
+      throw new ErrorsApp('Apenas professores podem cancelar avaliações', 400);
+    }
+
+    const exam = await this.examsRepository.findById(
+      examId,
+      schoolId,
+      teacher.id,
+    );
     if (!exam) {
-      throw new ErrorsApp('Exam does not exists', 404);
+      throw new ErrorsApp('Avaliação não encontrada', 404);
     }
 
     await this.examsRepository.delete(exam);
