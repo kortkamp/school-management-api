324322c5588d14bc331fb126c608b05cea691cc8
kortkamp <marcelusmedius@gmail.com>
2022-11-21 08:36:21 -0300

    fix: change param name

--- src/modules/classGroups/repositories/IClassGroupsRepository.ts
+++ src/modules/classGroups/repositories/IClassGroupsRepository.ts
@@ -6,7 +6,10 @@ interface IClassGroupsRepository {
   getAll(school_id: string, relations?: string[]): Promise<IClassGroup[]>;
   listClassGroups(school_id: string): Promise<IClassGroup[]>;
   getAllByTeacher(teacher_id: string): Promise<IClassGroup[]>;
-  findById(userId: string, school_id: string): Promise<IClassGroup | undefined>;
+  findById(
+    class_group_id: string,
+    school_id: string,
+  ): Promise<IClassGroup | undefined>;
   findByName(name: string): Promise<IClassGroup | undefined>;
   save(dataUpdate: IClassGroup): Promise<void>;
   delete(user: IClassGroup): Promise<void>;
